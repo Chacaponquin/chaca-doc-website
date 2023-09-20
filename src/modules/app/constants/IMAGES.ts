e64a164b7224890328cbebcef7fdd9dd39926af1
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-09-20 13:51:20 -0400

    Optimize images

--- src/modules/app/constants/IMAGES.ts
+++ src/modules/app/constants/IMAGES.ts
@@ -2,20 +2,20 @@ export const APP_IMAGES = {
    HEADER_IMAGE: { src: '/img/logo.png', alt: 'Chaca Logo' },
 
    EXPORT_EXTENSIONS: {
-      JAVA: { src: '/img/export_ext/java.png', alt: 'java' },
-      JSON: { src: '/img/export_ext/json.png', alt: 'json' },
-      CSV: { src: '/img/export_ext/csv.png', alt: 'csv' },
+      JAVA: { src: '/img/export_ext/java.webp', alt: 'java' },
+      JSON: { src: '/img/export_ext/json.webp', alt: 'json' },
+      CSV: { src: '/img/export_ext/csv.webp', alt: 'csv' },
       POSTGRESQL: {
-         src: '/img/export_ext/postgresql.png',
+         src: '/img/export_ext/postgresql.webp',
          alt: 'postgresql',
       },
-      TYPESCRIPT: { src: '/img/export_ext/ts.png', alt: 'typescript' },
+      TYPESCRIPT: { src: '/img/export_ext/ts.webp', alt: 'typescript' },
       JAVASCRIPT: { src: '/img/export_ext/js.png', alt: 'javascript' },
-      YAML: { src: '/img/export_ext/yaml.png', alt: 'yaml' },
-      PYTHON: { src: '/img/export_ext/python.png', alt: 'python' },
+      YAML: { src: '/img/export_ext/yaml.webp', alt: 'yaml' },
+      PYTHON: { src: '/img/export_ext/python.webp', alt: 'python' },
    },
 
-   SCHEMA_CODE: { src: '/img/schema_code.png', alt: 'Chaca Schema Code' },
-   API_CODE: { src: 'img/api_code.png', alt: 'Chaca Api Request Code' },
-   EXPORT_CODE: { src: 'img/export_code.png', alt: 'Chaca Export Code' },
+   SCHEMA_CODE: { src: '/img/schema_code.webp', alt: 'Chaca Schema Code' },
+   API_CODE: { src: 'img/api_code.webp', alt: 'Chaca Api Request Code' },
+   EXPORT_CODE: { src: 'img/export_code.webp', alt: 'Chaca Export Code' },
 } as const
